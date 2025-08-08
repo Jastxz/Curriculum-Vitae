a4bd966f5f4568c4b609ddde5689d4fc977841a9
Jastxz <javicraft14@gmail.com>
2025-08-08 21:03:34 +0200

    config: añadida opción de configuración para que los archivos distributables no repitan nombre

--- CV/vite.config.ts
+++ CV/vite.config.ts
@@ -15,4 +15,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        // Asegurar hash único en cada build
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
